cf50397541db4af4cb1f74837437b0d309e6f4fa
Victor Seleimend <vsennaseleimend@gmail.com>
2022-09-05 12:21:49 -0300

    single recipe fetch feature added

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = secs =>
@@ -7,3 +14,43 @@ const timeout = secs =>
       secs * 1000
     )
   );
+
+const showRecipes = async () => {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=e9a8ef4e-351a-405e-8f49-8a5e012f499e`
+    );
+
+    let {
+      message,
+      data: {
+        recipe: {
+          id,
+          title,
+          publisher,
+          source_url,
+          image_url,
+          servings,
+          cooking_time,
+          ingredients,
+        },
+      },
+    } = await res.json();
+
+    if (!res.ok) throw new Error(`${message} (${res.status})`);
+
+    recipe = {
+      id,
+      title,
+      publisher,
+      sourceUrl: source_url,
+      image: image_url,
+      servings,
+      cookingTime: cooking_time,
+      ingredients,
+    };
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipes();
